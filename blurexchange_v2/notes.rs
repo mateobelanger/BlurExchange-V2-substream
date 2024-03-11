000576ac1730cb82aeb7a71453904475e75beb2f
Mathieu Bélanger <m-belanger@outlook.com>
2024-03-11 13:30:06 -0400

    chore: clean repo

--- blurexchange_v2/notes.rs
+++
@@ -1,7 +0,0 @@
-/* substreams::log::info 
-
-TransactionTrace.logs_with_calls()
-
-
-1. generate rust strucs from the solidity structs
-2. Write helper code to map from the raw struct to the novel rust struct */
